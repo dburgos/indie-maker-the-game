f8afbb9ff92a9cd8765bade0fce7cdaf71bdb8b6
dburgos <david.burgos.risco@gmail.com>
2019-01-30 18:23:07 +0100

    Updates

--- precache-manifest.86f5000fd38bcad9080ff246fbfd50de.js
+++ precache-manifest.86f5000fd38bcad9080ff246fbfd50de.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/indie-maker-the-game/static/js/runtime~main.3ba4d759.js"
   },
   {
-    "revision": "c3d04f2039a3b6ba1b5d",
-    "url": "/indie-maker-the-game/static/js/main.c3d04f20.chunk.js"
+    "revision": "48da3e618c1ae0699529",
+    "url": "/indie-maker-the-game/static/js/main.48da3e61.chunk.js"
   },
   {
     "revision": "64eddeaaee108def94b6",
     "url": "/indie-maker-the-game/static/js/1.64eddeaa.chunk.js"
   },
   {
-    "revision": "c3d04f2039a3b6ba1b5d",
+    "revision": "48da3e618c1ae0699529",
     "url": "/indie-maker-the-game/static/css/main.739ee522.chunk.css"
   },
   {
-    "revision": "edd2f7cecc85317b86d40eb947716209",
+    "revision": "afff120a829d0fe3f22f56e5bfc8cf22",
     "url": "/indie-maker-the-game/index.html"
   }
 ];
