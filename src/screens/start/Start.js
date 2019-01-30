ec9d42a27dad90d57e35496f3eadbffea582fb7e
dburgos <david.burgos.risco@gmail.com>
2019-01-30 18:22:46 +0100

    Fix Play button route after deploy

--- src/screens/start/Start.js
+++ src/screens/start/Start.js
@@ -10,7 +10,7 @@ class Start extends Component {
         <h1><Emoji symbol="👨‍💻"/></h1>
         <h1>Indie Maker</h1>
         <h5>The Game</h5>
-        <Link to='/make' className="PlayBtn">Play</Link>
+        <Link to={`${process.env.PUBLIC_URL}/make`} className="PlayBtn">Play</Link>
       </div>
     );
   }
