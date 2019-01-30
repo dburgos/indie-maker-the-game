48bd50eb4bd4eade7c6b2186b7337ab96ce0990f
dburgos <david.burgos.risco@gmail.com>
2019-01-30 20:06:11 +0100

    Updates

--- precache-manifest.081b46c507f5dd1c428966c955f77919.js
+++ precache-manifest.081b46c507f5dd1c428966c955f77919.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/indie-maker-the-game/static/js/runtime~main.3ba4d759.js"
   },
   {
-    "revision": "01a926675446594d4283",
-    "url": "/indie-maker-the-game/static/js/main.01a92667.chunk.js"
+    "revision": "6ab557458c27c42174e7",
+    "url": "/indie-maker-the-game/static/js/main.6ab55745.chunk.js"
   },
   {
     "revision": "64eddeaaee108def94b6",
     "url": "/indie-maker-the-game/static/js/1.64eddeaa.chunk.js"
   },
   {
-    "revision": "01a926675446594d4283",
-    "url": "/indie-maker-the-game/static/css/main.f7e1b286.chunk.css"
+    "revision": "6ab557458c27c42174e7",
+    "url": "/indie-maker-the-game/static/css/main.f72f3ddf.chunk.css"
   },
   {
-    "revision": "8dbd81bf62fbc3262ec6326342ce5d0d",
+    "revision": "5c867ff0968762a63fdb2c557f28509d",
     "url": "/indie-maker-the-game/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/indie-maker-the-game/precache-manifest.6c92084824afefe95c9c3662d963d49c.js"
+  "/indie-maker-the-game/precache-manifest.081b46c507f5dd1c428966c955f77919.js"
 );
 
 workbox.clientsClaim();
